e308aa0667fa23da9e6f296b8a9c089c13372bc4
Person098123 <143832410+Person098123@users.noreply.github.com>
2024-02-08 10:41:29 -0800

    Accomplished nothing, and made absolutely no progress at all

--- code.js
+++ code.js
@@ -1,4 +1,25 @@
-function myFunction(){
-   var x = document.createElement("pics/basic certificate.png");
-   document.body.appendChild(x);
+
+
+function nFunction(){
+var x = document.createElement("IMG");
+x.setAttribute("src", "pics/basic certificate.png");
+certificate.getElementByClass("certificate-container")
+
+
+// if(nFunction==)
+//    element.remove( "pics/school-certificate.png");
+// else element.remove("pics/ribbon-certificate.png");  
+
 }
+function sFunction(){
+   var x = document.createElement("IMG");
+   x.setAttribute("src", "pics/school-certificate.png");
+   document.body.appendChild(x);
+   }
+
+   function mFunction(){
+      var x = document.createElement("IMG");
+      x.setAttribute("src", "pics/ribbon-certificate.png");
+      document.body.appendChild(x);
+      }
+
