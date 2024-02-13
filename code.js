d0741c79614545ab7cb5b1ae9c67eeeb8583965f
Person098123 <143832410+Person098123@users.noreply.github.com>
2024-02-13 09:26:14 -0800

    Fixed all the problems with the centering

--- code.js
+++ code.js
@@ -1,57 +1,57 @@
 
 
-function nFunction(){
-document.getElementById("certificate-container").style.backgroundImage="url('pics/basic-certificate.png')"; 
+function nFunction() {
+   document.getElementById("certificate-container").style.backgroundImage = "url('pics/basic2.png')";
 
 }
-function sFunction(){
-   document.getElementById("certificate-container").style.backgroundImage="url('pics/school-certificate.png')";
-   }
+function sFunction() {
+   document.getElementById("certificate-container").style.backgroundImage = "url('pics/school2.png')";
+}
 
-   function mFunction(){
-      document.getElementById("certificate-container").style.backgroundImage="url('pics/ribbon-certificate.png')";
-      }
+function mFunction() {
+   document.getElementById("certificate-container").style.backgroundImage = "url('pics/ribbon2.png')";
+}
 
-      function lFunction(){
-         document.getElementById("pick-l").style.backgroundImage="url('pics/ze-spy-from-tf2.png')";
-         }
-   
+function lFunction() {
+   document.getElementById("pick-l").style.backgroundImage = "url('pics/spy2.png')";
+}
 
-         function gFunction(){
-            document.getElementById("pick-l").style.backgroundImage="url('pics/gulpin.png')";
-            
-            }
-            function cwFunction(){
-               document.getElementById("pick-l").style.backgroundImage="url('pics/clovis-west.png')";
-               }
-               function wFunction(){
-                  document.getElementById("pick-l").style.backgroundImage="url('pics/walt.png')";
-                  
-                  }
-                  function bFunction(){
-                     document.getElementById("pick-medel").style.backgroundImage="url('pics/blue-removebg-preview.png')"; 
-                     
-                     }
-                     function rFunction(){
-                        document.getElementById("pick-medel").style.backgroundImage="url('pics/red-removebg-preview.png')";
-                        }
-                     
-                        function goFunction(){
-                           document.getElementById("pick-medel").style.backgroundImage="url('pics/gold-removebg-preview.png')";
-                           }
 
-                           function textFunction() {
-                              var x = document.getElementById("text").value;
-                              document.getElementById("put-text").innerHTML = x;
-                            }
+function gFunction() {
+   document.getElementById("pick-l").style.backgroundImage = "url('pics/gulpin2.png')";
 
-                            function forFunction() {
-                              var x = document.getElementById("for-value").value;
-                              document.getElementById("why").innerHTML = x;
+}
+function cwFunction() {
+   document.getElementById("pick-l").style.backgroundImage = "url('pics/cw2.png')";
+}
+function wFunction() {
+   document.getElementById("pick-l").style.backgroundImage = "url('pics/walt2.png')";
 
-      
-                            }
-                              function dateFunction() {
-                              const d = new Date();
-                              document.getElementById("thedate").innerHTML = d.toDateString();
-                              }
+}
+function bFunction() {
+   document.getElementById("pick-medel").style.backgroundImage = "url('pics/blue2.png')";
+
+}
+function rFunction() {
+   document.getElementById("pick-medel").style.backgroundImage = "url('pics/red2.png')";
+}
+
+function goFunction() {
+   document.getElementById("pick-medel").style.backgroundImage = "url('pics/gold2.png')";
+}
+
+function textFunction() {
+   var x = document.getElementById("text").value;
+   document.getElementById("put-text").innerHTML = x;
+}
+
+function forFunction() {
+   var x = document.getElementById("for-value").value;
+   document.getElementById("why").innerHTML = x;
+
+
+}
+function dateFunction() {
+   const d = new Date();
+   document.getElementById("thedate").innerHTML = d.toDateString();
+}
